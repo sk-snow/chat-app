a4ff11948d954be1d62f462d807be28e073bcef6
sk-snow <cushion_mfnk29@outlook.jp>
2024-02-18 15:58:31 +0900

    deviseに関するビューファイルの配置

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_17_105407) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_18_060657) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
