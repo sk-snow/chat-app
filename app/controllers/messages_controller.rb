c6068aec1e1625225873a67b30215e24016a5f35
sk-snow <cushion_mfnk29@outlook.jp>
2024-02-22 21:39:29 +0900

    空のメッセージが送信できないバリデーションの設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,7 +12,7 @@ class MessagesController < ApplicationController
       redirect_to room_messages_path(@room)
     else
       @messages = @room.messages.includes(:user)
-      render :indes, satus: :unprocessable_entity
+      render :index, satus: :unprocessable_entity
     end
   end
 
